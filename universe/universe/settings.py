a1602d68c5a486a907253f5257969f6e98f5bc05
Zues Fugitive <stephaineanspach@gmail.com>
2021-05-18 19:23:16 +0100

    Next 1

--- universe/universe/settings.py
+++ universe/universe/settings.py
@@ -189,5 +189,5 @@ REST_FRAMEWORK = {
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bloghub274@gmail.com'
+EMAIL_HOST_USER = 'test123@gmail.com'
 EMAIL_HOST_PASSWORD = ''
